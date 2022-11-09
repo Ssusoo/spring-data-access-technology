d34731711782e572ec6c23376a9cbd2852995d63
김수환 <zzanggoon8@hiconsy.com>
2022-11-09 11:01:44 +0900

    first commit

---
+++ src/main/java/me/ssu/springdataaccesstechnology/SpringDataAccessTechnologyApplication.java
@@ -0,0 +1,13 @@
+package me.ssu.springdataaccesstechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAccessTechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAccessTechnologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ssu/springdataaccesstechnology/SpringDataAccessTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.springdataaccesstechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAccessTechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
